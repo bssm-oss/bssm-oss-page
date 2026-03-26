12e92ca0a9e81d104783a97410ac8496399f478d
heodongun <heodongun0922@gmail.com>
2026-03-26 16:39:10 +0900

    test: cover shared live editor workspace

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -3,5 +3,7 @@ import { cleanup } from '@testing-library/react'
 import { afterEach } from 'vitest'
 
 afterEach(() => {
+  const storage = window.localStorage as { clear?: () => void } | undefined
+  storage?.clear?.()
   cleanup()
 })
